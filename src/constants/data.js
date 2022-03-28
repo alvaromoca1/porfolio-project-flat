04d41c3d1c66ce1994c5ac20e43fb358ff2967a4
alvaromoca1 <alvaromoca1@gmail.com>
2022-03-27 20:48:54 -0500

    agregando una arquitectura de archivos adecuados y la data

---
+++ src/constants/data.js
@@ -0,0 +1,68 @@
+
+const projects=[
+    {
+        title:"este es el primer titulo de un projecto 1",
+        description:"Esta es ua descripcion que se debe tener, creo que se tiene que tener como ejemplo",
+        link:"https://www.examplecom",
+        img:""
+    },
+    {
+        title:"este es el primer titulo de un projecto 2",
+        description:"Esta es ua descripcion que se debe tener, creo que se tiene que tener como ejemplo",
+        link:"https://www.examplecom",
+        img:""
+    },
+    {
+        title:"este es el primer titulo de un projecto 3",
+        description:"Esta es ua descripcion que se debe tener, creo que se tiene que tener como ejemplo",
+        link:"https://www.examplecom",
+        img:""
+    },
+    {
+        title:"este es el primer titulo de un projecto 4",
+        description:"Esta es ua descripcion que se debe tener, creo que se tiene que tener como ejemplo",
+        link:"https://www.examplecom",
+        img:""
+    },
+];
+
+const articles=[
+    {
+        date:"12/12/2020",
+        title:"Este es un article que se podr aponer en el ",
+        sumary:"como es china, bueno en la tierra de china hayt muchas personas",
+        link:"https://www.examplecom"
+    },
+    {
+        date:"12/12/2020",
+        title:"Este es un article que se podr aponer en el 2 ",
+        sumary:"como es china, bueno en la tierra de china hayt muchas personas",
+        link:"https://www.examplecom"
+    },
+    {
+        date:"12/12/2020",
+        title:"Este es un article que se podr aponer en el 3",
+        sumary:"como es china, bueno en la tierra de china hayt muchas personas",
+        link:"https://www.examplecom"
+    },
+    {
+        date:"12/12/2020",
+        title:"Este es un article que se podr aponer en el 4 ",
+        sumary:"como es china, bueno en la tierra de china hayt muchas personas",
+        link:"https://www.examplecom"
+    }
+]
+const socialMedia=[
+    {
+        title:"facebook",
+        icon:"algo",
+        link:"https://www.examplecom"
+    },
+    {
+        title:"google",
+        icon:"algo",
+        link:"https://www.examplecom"
+    }
+]
+
+export default {projects,articles,socialMedia}
